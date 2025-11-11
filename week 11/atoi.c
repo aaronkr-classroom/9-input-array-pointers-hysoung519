d5f13b3d47ed4a3ed7d3d3cb966f209687994be1
임현성 <mallong.leen@gmail.com>
2025-11-11 17:13:20 +0900

    finish!!!!!!!!!!

---
+++ week 11/atoi.c
@@ -0,0 +1,29 @@
+﻿//atoi.c
+#include <stdio.h>
+#include <conio.h>
+
+#define STR_MAX 16
+
+
+
+int main()
+{
+	int first_num, second_num;
+	char first_str[STR_MAX], second_str[STR_MAX];
+
+	printf("1st num: ");
+	fgets(first_str, STR_MAX, stdin);
+	printf("2nd num: ");
+	fgets(second_str, STR_MAX, stdin);
+
+	//printf("문자합:\n"); 못해요 ㅜㅜOTL
+	//printf("%s + %s = %s", first_str, second_str, first_str + second_str);
+
+
+	first_num = atoi(first_str);
+	second_num = atoi(second_str);
+
+	printf("%d + %d = %d\n", first_num, second_num, first_num + second_num);
+
+	return 0;
+}
